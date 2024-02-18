2e815fa812b64af06607750b0972fc22646bc630
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-02-19 03:02:07 +0600

    set the button activation functionalities and seat booking function

--- Scripts/first.js
+++ Scripts/first.js
@@ -7,22 +7,50 @@ const allTicketSelectBtn=document.getElementsByClassName('ticket-select-btn');
 for(let ticketSelectBtn of allTicketSelectBtn){
     let clicked=true;
     let counterArray=[];
+    let count=0;
     ticketSelectBtn.addEventListener('click',function(e){
         if(clicked){
+            ticketSelectBtn.style.backgroundColor='#1DD100'
+            ticketSelectBtn.style.color='white'
+            ticketSelectBtn.style.cursor='not-allowed'
             ticketSelectBtn.disabled=true;
         }
         const totalCurrentSeatQuantity=document.getElementById('current-seat-quantity');
         let totalCurrentSeatQuantityNum=parseInt(totalCurrentSeatQuantity.innerText);
         totalCurrentSeatQuantityNum-=1;
-        
         totalCurrentSeatQuantity.innerText=totalCurrentSeatQuantityNum;
+
+        const totalSoldSeat=document.getElementById('sold-seat');
+        let totalSoldSeatNum=parseInt(totalSoldSeat.innerText);
+        totalSoldSeatNum+=1;
+        totalSoldSeat.innerText=totalSoldSeatNum;
+        count=totalSoldSeatNum;
+        if(count===4){
+            const applyBtn=document.getElementById('apply-btn');
+            applyBtn.removeAttribute('disabled'); 
+        }
+
         if (totalCurrentSeatQuantityNum === 0) {
             for(let disabledCurrentSeat of allTicketSelectBtn)
             disabledCurrentSeat.disabled = true;
         }
-        counterArray.push(totalCurrentSeatQuantityNum);
-        console.log(counterArray);
-
+        activateNextButton(count);
     })
-    console.log(counterArray);
+}
+
+function activateNextButton(count) {
+    const inputNumber = document.getElementById('input-number');
+    console.log(inputNumber);
+    const nextButton = document.getElementById('btn-next');
+    console.log(activateNextButton);
+
+    inputNumber.addEventListener('input', function () {
+        const inputValue = inputNumber.value.trim();
+        const isNumeric = /^\d+$/.test(inputValue);
+        if (isNumeric && count === 1) {
+            nextButton.removeAttribute('disabled');
+        } else {
+            nextButton.setAttribute('disabled', 'disabled');
+        }
+    });
 }
